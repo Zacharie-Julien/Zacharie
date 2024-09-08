8cbf9bd9eb3974311c2a2c9e97217ee2a234a4a6
Zacharie Julien <zacharie.julien@outlook.com>
2024-09-08 11:21:41 +0200

    Portfolio version 0.1.0, pages actives : Acceuil Présentation Formation Expériences

---
+++ Js/script.js
@@ -0,0 +1,164 @@
+const text1_options = [
+    "Un vrai passioné ?",
+    "Un pannel de compétences qui évolue",
+    "Une formation adéquat",
+    "De multiple projet personnel"
+  ];
+  const text2_options = [
+    "69 min. read",
+    "7 min. read",
+    "8 min. read",
+    "87,658.1277 min. read"
+  ];
+  const color_options = ["#EBB9D2", "#FE9968", "#7FE0EB", "#6CE5B1"];
+  const image_options = [
+    "../Pictures/vangohg.webp",
+    "../Pictures/paysage.webp",
+    "../Pictures/expérienceVangHog.webp",
+    "https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
+  ];
+  var i = 0;
+  const currentOptionText1 = document.getElementById("current-option-text1");
+  const currentOptionText2 = document.getElementById("current-option-text2");
+  const currentOptionImage = document.getElementById("image");
+  const carousel = document.getElementById("carousel-wrapper");
+  const mainMenu = document.getElementById("menu");
+  const optionPrevious = document.getElementById("previous-option");
+  const optionNext = document.getElementById("next-option");
+  
+  currentOptionText1.innerText = text1_options[i];
+  currentOptionText2.innerText = text2_options[i];
+  currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+  mainMenu.style.background = color_options[i];
+  
+  function fonctionNext() {
+    i = i + 1;
+    i = i % text1_options.length;
+    currentOptionText1.dataset.nextText = text1_options[i];
+  
+    currentOptionText2.dataset.nextText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-next");
+    
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-next");
+    }, 650);
+  };
+  
+
+  function fonctionPrevious() {
+    if (i === 0) {
+      i = text1_options.length;
+    }
+    i = i - 1;
+    currentOptionText1.dataset.previousText = text1_options[i];
+  
+    currentOptionText2.dataset.previousText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-previous");
+  
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-previous");
+    }, 650);
+  };
+  
+
+setInterval(fonctionNext, 20000);
+
+
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = '!<>-_\\/[]{}—=+*^?#________'
+      this.update = this.update.bind(this)
+    }
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+  
+  // ——————————————————————————————————————————————————
+  // Example
+  // ——————————————————————————————————————————————————
+  
+  const phrases = [
+    'QUI SUIS-JE ?',
+    'DEVELOPPEUR',
+    'PASSIONNE',
+    'ETUDIANT'
+  ]
+  
+  const el = document.querySelector('.scrambling')
+  const fx = new TextScramble(el)
+  
+  let counter = 0
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 800)
+    })
+    counter = (counter + 1) % phrases.length
+  }
+  
+  next()
